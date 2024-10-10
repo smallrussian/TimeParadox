effa9e01256467a74571e4080a0f9d878f94cf5c
Kameron Bustos <kbustos31@outlook.com>
2024-10-10 18:56:01 -0500

    added push interfaces for moving the generator

---
+++ Source/TimeParadox/Generator.cpp
@@ -0,0 +1,69 @@
+﻿#include "Generator.h"
+#include "TimeParadoxCharacter.h"
+#include "Components/StaticMeshComponent.h"
+#include "GameFramework/PlayerController.h"
+#include "TimeParadoxPlayerController.h"
+// Sets default values
+AGenerator::AGenerator()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Create and set up the generator's static mesh component
+	GeneratorMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("GeneratorMesh"));
+	GeneratorMesh->SetSimulatePhysics(true); // Enable physics simulation so it can be pushed around
+	RootComponent = GeneratorMesh;
+
+	PushSpeed = 200.0f; // Default speed for pushing the generator
+	bIsBeingPushed = false;
+	bPushed = false;
+}
+
+// Called when the game starts or when spawned
+void AGenerator::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void AGenerator::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	// If the generator is being pushed, move it forward based on player's input
+	if (bIsBeingPushed && PushingPlayer)
+	{
+		FVector ForwardDirection = PushingPlayer->GetActorForwardVector();
+		GeneratorMesh->AddForce(ForwardDirection * PushSpeed * GeneratorMesh->GetMass());
+	}
+}
+
+// Implement the interaction interface's ActivateActor function
+void AGenerator::ContinuousActivateActor(ATimeParadoxCharacter player*)
+{
+	PushingPlayer = player;
+	if (bIsBeingPushed)
+	{
+		// Start pushing the generator
+		ATimeParadoxPlayerController* PlayerController = Cast<ATimeParadoxPlayerController>(PushingPlayer->GetController());
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, TEXT("You PUSHED THE GENERATOR!"));
+
+		if (PlayerController)
+		{
+			bIsBeingPushed = true;
+			PushingPlayer = Cast<ATimeParadoxCharacter>(PlayerController->GetPawn());
+		}
+	}
+	else
+	{
+		// Stop pushing the generator
+		StopPushing();
+	}
+}
+
+// Stop pushing the generator
+void AGenerator::StopPushing()
+{
+	bIsBeingPushed = false;
+	PushingPlayer = nullptr;
+}
+void ActivateActor(ATimeParadoxCharacter*){}
---
+++ Source/TimeParadox/Generator.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CPP_InteractionInterface.h"
+#include "Generator.generated.h"
+
+UCLASS(Blueprintable)  // Make the class Blueprintable so it can be extended in Blueprints
+class TIMEPARADOX_API AGenerator : public AActor, public ICPP_PushInterface
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AGenerator();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Implement interaction interface
+	virtual void ActivateActor(ATimeParadoxCharacter*) override;
+	
+private:
+	// Static mesh component for the generator (for collision and visuals)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* GeneratorMesh;
+
+	// Boolean to track if the generator is being pushed
+	UPROPERTY(BlueprintReadOnly, Category = "State", meta = (AllowPrivateAccess = "true"))
+	bool bIsBeingPushed;
+
+	// Reference to the player pushing the generator
+	UPROPERTY()
+	class ATimeParadoxCharacter* PushingPlayer;
+
+	// Speed at which the generator is pushed
+	float PushSpeed;
+
+	// bool
+	bool bPushed;
+	// End pushing logic
+	void StopPushing();
+};
